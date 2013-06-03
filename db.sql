a1ed01909a8601225a73ba46d5774189c3e3226b
Camilo Uribe <camilo.uribe@gmail.com>
2013-06-02 20:26:03 -0500

    listado de eps

--- db.sql
+++ db.sql
@@ -4,3 +4,5 @@ CREATE TABLE eps (
   calificacion INT NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO `eps` VALUES (1,'Compensar',0),(2,'Famisanar',0),(3,'Colmedica',0),(4,'Humana Vivir',0),(5,'Salud Coop',0),(6,'Coomeva ',0),(7,'Cruz Blanca',0),(8,'Saludvida',0),(9,'Red Salud',0),(10,'Salud Colpatria',0),(11,'Salud Total ',0),(12,'Solsalud',0),(13,'Sanitas',0),(14,'Cafesalud',0),(15,'SOS',0),(16,'Golden Group',0);
